054c5b1fe91c289cf6636c2438343fde08b48d31
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2025-03-29 22:34:11 -0300

    feat(frontend): criar componentes Favorite, SearchProfile e CreatePost, e pequenas alterações no design
    - Criar componente Favorite para mostrar os posts salvos
    - Criar componente SearchProfile para procurar por pessoas
    - Criar componente CreatePost para criar um novo post
    - Fazer pequenas alterações no design do componente Aside e outros componentes

---
+++ newgram-ui/src/app/explore/explore.component.ts
@@ -0,0 +1,194 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { PostDetailsComponent } from '../post-details/post-details.component';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-explore',
+  imports: [CommonModule, PostDetailsComponent],
+  templateUrl: './explore.component.html',
+  styleUrl: './explore.component.css'
+})
+export class ExploreComponent {
+  constructor(private title:Title) {
+    this.title.setTitle('Explore');
+  }
+  postSelected: any = null;
+  selectedIndex: any = null;
+  selectedPost: any = null;
+  selectedPostType: string = '';
+  viralPosts = [
+  {
+    id: 1,
+    author: 'Whindersson Nunes',
+    username: '@whindersson',
+    avatar: 'https://s2-oglobo.glbimg.com/LaW6NoqTlik3XAzENbU6WZrVLaI=/0x0:651x562/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2024/V/x/z1C221T4i0bNRAYNDNAA/whatsapp-image-2024-09-26-at-17.13.21.jpeg',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2gIwo0mtbJnUYwJ9dqsWivE7MuBklwV8FVeII-lQrEfcfAnQk0nt10pN3RxbCrzWXnMM&usqp=CAU',
+    text: 'Treino pesado hoje! Quem mais tá na luta? 💪 #Fitness',
+    location: 'São Paulo',
+    tags: ['Humor', 'Esportes', 'VidaSaudável'],
+    likes: 1250000,
+    comments: 32400,
+    shares: 87200,
+    time: '2h'
+  },
+  {
+    id: 2,
+    author: 'Paulo Muzy',
+    username: '@paulomuzy',
+    avatar: 'https://www.dialethoseventos.com.br/assets-custom/img/palestrantes/paulo-muzy-12072023-164852.jpg',
+    image: 'https://i.ytimg.com/vi/6r1mxVMfY1o/maxresdefault.jpg',
+    text: 'Dica de nutrição pós-treino que mudou meus resultados!',
+    location: 'São Rio de Janeiro',
+    tags: ['Fitness', 'Nutrição', 'Saúde'],
+    likes: 890000,
+    comments: 15400,
+    shares: 32100,
+    time: '5h'
+  },
+  {
+    id: 3,
+    author: 'Gustavo Lima',
+    username: '@gusttavo_lima',
+    avatar: 'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/09/imagem-29-1.jpg?w=1200&h=1200&crop=1',
+    image: 'https://uploads.maisgoias.com.br/2024/05/168ce51f-que-calor-7.jpg',
+    text: 'Turnê "Buteco do Gusttavo" esgotada em 3 cidades! Obrigado, fãs! 🎶',
+    location: 'Pernambuco',
+    tags: ['Sertanejo', 'Show', 'Música'],
+    likes: 2100000,
+    comments: 45000,
+    shares: 92000,
+    time: '4h'
+  }
+];
+
+celebrityPosts = [
+  {
+    id: 1,
+    author: 'Ivete Sangalo',
+    username: '@ivetesangalo',
+    avatar: 'https://cdn-images.dzcdn.net/images/artist/fb27c1806a4b63a9633da56f57ca5fd0/1900x1900-000000-80-0-0.jpg',
+    image: 'https://i.ytimg.com/vi/13MqkelD09E/maxresdefault.jpg',
+    text: 'Preparem-se para o Carnaval 2024! #Axé #Bahia',
+    location: 'Bahia',
+    tags: ['Música', 'Carnaval', 'Show'],
+    likes: 3200000,
+    comments: 42800,
+    shares: 156000,
+    time: '1d'
+  },
+  {
+    id: 2,
+    author: 'Rodrigo Faro',
+    username: '@rodrigofaro',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF7C6VEkzSCfwkduTPL5CYmII-kAIKJvIX2Q&s',
+    image: 'https://ofuxico.com.br/wp-content/uploads/2024/12/rodrigo-faro-1.jpg',
+    text: 'Domingo em família é tudo de bom! ❤️',
+    location: 'Rio de Janeiro',
+    tags: ['Família', 'Domingão'],
+    likes: 1870000,
+    comments: 38200,
+    shares: 45600,
+    time: '1d'
+  },
+  {
+    id: 3,
+    author: 'Xororó',
+    username: '@xororooficial',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnXJyDrgbacogHWuyraAHsspig6BtHFEWcRQ&s',
+    image: 'https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2022/05/31/615x300/1_chitaozinhoexororo-21133146.jpg',
+    text: 'Sertanejo a noite toda! 🎶',
+    location: 'Rio Grande do Sul',
+    tags: ['Música', 'Sertanejo', 'Tradição'],
+    likes: 950000,
+    comments: 18000,
+    shares: 25000,
+    time: '1d'
+  }
+];
+
+followedPeoplePosts = [
+  {
+    id: 1,
+    author: 'Sabrina Sato',
+    username: '@sabrinasato',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqrHq1mkNqRoi8iRLAHNddoOnse6PDpu7RIw&s',
+    image: 'https://s2-marieclaire.glbimg.com/yzsSkSQNIi1_ZqVIaJsEKTvGQYc=/0x0:4088x2300/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_51f0194726ca4cae994c33379977582d/internal_photos/bs/2025/y/W/uBn54gTHCUKIexhEAe3Q/befunky-collage-58-.jpg',
+    text: 'Vistas incríveis de Gramado! Quem já veio?',
+    location: 'Rio Grande do Norte',
+    tags: [' #Viagem', ' #Turismo', ' #RioGrandeDoSul'],
+    likes: 456000,
+    comments: 12800,
+    shares: 9800,
+    time: '3h'
+  },
+  {
+    id: 2,
+    author: 'Marcos Mion',
+    username: '@marcosmion',
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Marcos_Mion_2021.jpg',
+    image: 'https://anotabahia.com/wp-content/uploads/2025/01/anotabahia-marcos-mion-fala-sobre-relacao-do-filme-mma-meu-melhor-amigo-com-sua-paternidade-whatsapp-image-2025-01-09-at-20.00.57-1200x900.jpeg',
+    text: 'Noite incrível no Prêmio Multishow!',
+    location: 'Espirito Santo',
+    tags: [' #Evento', ' #TV', ' #Entretenimento'],
+    likes: 678000,
+    comments: 21500,
+    shares: 12400,
+    time: '8h'
+  },
+  {
+    id: 3,
+    author: 'Sandy',
+    username: '@sandy',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVxidCuln_ATp1mgP8usIEK0A6xhJopGo4pg&s',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdqKgjmu6iM2Eglf3CCiB2vLnNmlvWiuBWSQ&s',
+    text: 'Novo single saindo semana que vem! Preparem-se 🎤',
+    location: 'Minas Gerais',
+    tags: [' #Música', ' #Lançamento', ' #MPB'],
+    likes: 870000,
+    comments: 22000,
+    shares: 31000,
+    time: '8h'
+  }
+];
+popularTags = [
+  'Humor',
+  'Esportes',
+  'VidaSaudável',
+  'Música',
+  'Carnaval',
+  'Show',
+  'Família',
+  'Domingão',
+  'Viagem',
+  'Turismo',
+  'RioGrandeDoSul',
+  'Evento',
+  'TV',
+  'Entretenimento'
+]
+openModal(post: any, index: number) {
+  this.postSelected = post;
+  this.selectedIndex = index;
+
+
+}
+openPostDetails(post: any, event: Event) {
+  this.postSelected = post;
+}
+
+getModalId(): string {
+  return `modal-${this.selectedPostType}-${this.selectedIndex}`;
+}
+formatNumber(count:any) {
+  if (count >= 10000000) {
+    return `${Math.floor(count / 1000000)}M`; // Ex: 28.700.000 → "28M"
+  } else if (count >= 1000000) {
+    return `${(count / 1000000).toFixed(1)}M`; // Ex: 1.500.000 → "1.5M"
+  } else if (count >= 1000) {
+    return `${Math.floor(count / 1000)}K`; // Ex: 150.000 → "150K"
+  } else {
+    return count.toString(); // Menos de 1.000
+  }
+}
+}
---
+++ newgram-ui/src/app/favorite/favorite.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  imports: [CommonModule],
+  templateUrl: './favorite.component.html',
+  styleUrl: './favorite.component.css'
+})
+export class FavoriteComponent {
+  savedPosts = [
+    {
+      id: 1,
+      author: 'Whindersson Nunes',
+      username: '@whindersson',
+      avatar: 'https://s2-oglobo.glbimg.com/LaW6NoqTlik3XAzENbU6WZrVLaI=/0x0:651x562/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2024/V/x/z1C221T4i0bNRAYNDNAA/whatsapp-image-2024-09-26-at-17.13.21.jpeg',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2gIwo0mtbJnUYwJ9dqsWivE7MuBklwV8FVeII-lQrEfcfAnQk0nt10pN3RxbCrzWXnMM&usqp=CAU',
+      text: 'Treino pesado hoje! Quem mais tá na luta? 💪 #Fitness',
+      location: 'São Paulo',
+      tags: ['Humor', 'Esportes', 'VidaSaudável'],
+      likes: 1250000,
+      comments: 32400,
+      shares: 87200,
+      time: '2h'
+    },
+    {
+      id: 2,
+      author: 'Paulo Muzy',
+      username: '@paulomuzy',
+      avatar: 'https://www.dialethoseventos.com.br/assets-custom/img/palestrantes/paulo-muzy-12072023-164852.jpg',
+      image: 'https://i.ytimg.com/vi/6r1mxVMfY1o/maxresdefault.jpg',
+      text: 'Dica de nutrição pós-treino que mudou meus resultados!',
+      location: 'São Rio de Janeiro',
+      tags: ['Fitness', 'Nutrição', 'Saúde'],
+      likes: 890000,
+      comments: 15400,
+      shares: 32100,
+      time: '5h'
+    },
+    {
+      id: 3,
+      author: 'Gustavo Lima',
+      username: '@gusttavo_lima',
+      avatar: 'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/09/imagem-29-1.jpg?w=1200&h=1200&crop=1',
+      image: 'https://uploads.maisgoias.com.br/2024/05/168ce51f-que-calor-7.jpg',
+      text: 'Turnê "Buteco do Gusttavo" esgotada em 3 cidades! Obrigado, fãs! 🎶',
+      location: 'Pernambuco',
+      tags: ['Sertanejo', 'Show', 'Música'],
+      likes: 2100000,
+      comments: 45000,
+      shares: 92000,
+      time: '4h'
+    }
+  ];
+  formatNumber(count:any) {
+    if (count >= 10000000) {
+      return `${Math.floor(count / 1000000)}M`; // Ex: 28.700.000 → "28M"
+    } else if (count >= 1000000) {
+      return `${(count / 1000000).toFixed(1)}M`; // Ex: 1.500.000 → "1.5M"
+    } else if (count >= 1000) {
+      return `${Math.floor(count / 1000)}K`; // Ex: 150.000 → "150K"
+    } else {
+      return count.toString(); // Menos de 1.000
+    }
+  }
+}
